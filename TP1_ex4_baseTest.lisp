979eded2e8f4d437f0a82a19a5af41b808e8e3bb
Leo Przewlocki <leo.przewlocki@etu.utc.fr>
2019-10-01 23:34:30 +0200

    add of Ex4_A BaseTest

---
+++ TP1_ex4_baseTest.lisp
@@ -0,0 +1,392 @@
+(setq BaseTest
+    ‘(
+        (
+            "Campagnes de Clovis Ier"
+            486
+            508
+            (
+                (
+                    "Royaume Franc"
+                )
+                (
+                    "Soissons"
+                    "Royaume alaman"
+                    "Royaume des Burgondes"
+                    "Royaume wisigoth"
+                    "Royaume ostrogoth"
+                    "Royaume wisigoth"
+                )
+            )
+            (
+                "Soissons"
+                "Zülpich"
+                "Dijon"
+                "Vouillé"
+                "Arles"
+            )
+        )
+        (
+            "Guerre de Burgondie"
+            523
+            533
+            (
+                (
+                    "Royaume Franc"
+                )
+                (
+                    "Royaume des Burgondes"
+                )
+            )
+            (
+                "Vezeronce"
+                "Arles"
+            )
+        )
+        (
+            "Conquête de la Thuringe"
+            531
+            531
+            (
+                (
+                    "Royaume Franc"
+                )
+                (
+                    "Thuringes"
+                )
+            )
+            (
+                "Thuringe"
+            )
+        )
+        (
+            "Guerre des Goths"
+            535
+            553
+            (
+                (
+                    "Royaume ostrogoth"
+                )
+                (
+                    "Empire byzantin"
+                )
+            )
+            (
+                "Péninsule italienne"
+            )
+        )
+        (
+            "Conquête de l'Alémanie"
+            536
+            536
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Alamans"
+                )
+            )
+            (
+                "Alémanie"
+            )
+        )
+        (
+            "Conquête de la Bavière"
+            555
+            555
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Bavarii"
+                )
+            )
+            (
+                "Bavière"
+            )
+        )
+        (
+            "Campagnes de Bretagne"
+            560
+            578
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Royaume du Vannetais"
+                )
+            )
+            (
+                "Vannetais"
+            )
+        )
+        (
+            "Guerre de succession mérovingienne"
+            584
+            585
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Royaume d'Aquitaine"
+                )
+            )
+            (
+                "Comminges"
+            )
+        )
+        (
+            "Guerre franco-frisonne"
+            600
+            793
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Royaume de Frise"
+                )
+            )
+            (
+                "Pays-Bas"
+                "Allemagne"
+            )
+        )
+        (
+            "Guerre civile des Francs"
+            715
+            719
+            (
+                (
+                    "Neustrie"
+                )
+                (
+                    "Austrasie"
+                )
+            )
+            (
+                "Royaume Franc"
+            )
+        )
+        (
+            "Invasion omeyyade en France"
+            719
+            759
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Califat omeyyade"
+                )
+            )
+            (
+                "Royaume d'Aquitaine"
+                "Septimanie"
+            )
+        )
+        (
+            "Guerre des Lombards"
+            755
+            758
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Lombards"
+                )
+            )
+            (
+                "Lombardie"
+            )
+        )
+        (
+            "Guerre d'Aquitaine"
+            761
+            768
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Aquitains"
+                )
+            )
+            (
+                "Vasconie Aquitaine"
+            )
+        )
+        (
+            "Guerre des Saxons"
+            772
+            804
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Saxons"
+                )
+            )
+            (
+                "Germanie"
+            )
+        )
+        (
+            "Guerre des Lombards"
+            773
+            774
+            (
+                (
+                    "Royaume franc"
+                )
+                (
+                    "Lombards"
+                )
+            )
+            (
+                "Lombardie"
+            )
+        )
+        (
+            "Guerre des Avars"
+            791
+            805
+            (
+                (
+                    "Royaume de France"
+                )
+                (
+                    "Avars"
+                )
+            )
+            (
+                "Pannonie"
+            )
+        )
+        (
+            "Invasions sarrasines en Provence"
+            798
+            990
+            (
+                (
+                    "Royaume de France"
+                    "Comité de Provence"
+                )
+                (
+                    "Sarrasins"
+                )
+            )
+            (
+                "Provence"
+            )
+        )
+        (
+            "Luttes inter-dynastiques carolingiennes"
+            830
+            842
+            (
+                (
+                    "Francie occidentale"
+                    "Francie orientale"
+                )
+                (
+                    "Francie médiane"
+                )
+            )
+            (
+                "Fontenoy"
+            )
+        )
+        (
+            "Guerre franco-bretonne"
+            843
+            851
+            (
+                (
+                    "Royaume de France"
+                )
+                (
+                    "Royaume de Bretagne"
+                    "Vikings"
+                )
+            )
+            (
+                "Royaume de Bretagne"
+            )
+        )
+        (
+            "Luttes inter-dynastiques carolingiennes"
+            876
+            946
+            (
+                (
+                    "Francie occidentale"
+                    "Francie orientale"
+                )
+                (
+                    "Royaume de Bourgogne"
+                    "Francie orientale"
+                )
+            )
+            (
+                "Ardennes"
+                "Saône-et-Loire"
+                "Rhénanie-Palatinat"
+                "Aisne"
+            )
+        )
+        (
+            "Invasions vikings en France"
+            799
+            1014
+            (
+                (
+                    "Royaume de France"
+                )
+                (
+                    "Vikings"
+                )
+            )
+            (
+                "Normandie"
+                "Bretagne"
+            )
+        )
+        (
+            "Première croisade"
+            1096
+            1099
+            (
+                (
+                    "Comté de Blois"
+                    "Comté de Toulouse"
+                    "Comté de Boulogne"
+                    "Marquisat de Provence"
+                    "Comté de Flandre"
+                    "Duché de Normandie"
+                    "Diocèse du Puy-en-Velay"
+                    "Comté de Vermandois"
+                    "République de Gênes"
+                    "Duché de Basse-Lotharingie"
+                    "Principauté de Tarente"
+                    "Empire byzantin"
+                    "Royaume de Petite-Arménie"
+                    "Croisés"
+                    "Royaume de France"
+                )
+                (
+                    "Sultanat de Roum Danichmendides"
+                    "Califat fatimide"
+                )
+            )
+            (
+                "Terre Sainte"
+            )
+        )
+    )
+)
