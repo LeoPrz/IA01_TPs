6c8ccb409e19427dd4d3801a9602ce61659c4dbd
Leo Przewlocki <leo.przewlocki@etu.utc.fr>
2019-10-01 23:56:44 +0200

    TP1 Ex4 B ok

--- TP1/TP1.lisp
+++ TP1/TP1.lisp
@@ -6,4 +6,30 @@
     (mapcar (lambda (n) (list n (* 3 n))) x)
 )
 
+; EXO 3 -------------------------------------------------
+
+; EXO 4 -------------------------------------------------
+; (A) cf. TP1_ex4_baseTest.lisp
+
+; (B)
+
+(defun dateDebut (conflit)
+    (cadr conflit)
+)
+
+(defun nomConflit (conflit)
+    (car conflit)
+)
+
+(defun allies (conflit)
+    (car (cadddr conflit))
+)
+
+(defun ennemis (conflit)
+    (cadr (cadddr conflit))
+)
+
+(defun lieu (conflit)
+    (nthcdr 4 conflit)
+)
 
