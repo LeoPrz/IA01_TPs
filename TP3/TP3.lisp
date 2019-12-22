9ab273c8dd56886f8662e87ddef11346db36f271
Alexandre Touzeau <alexandre.touzeau@etu.utc.fr>
2019-12-22 17:11:01 +0100

    Add rules and descript them in .pages

--- TP3/TP3.lisp
+++ TP3/TP3.lisp
@@ -5,17 +5,26 @@
   (R3 ((valeur justice) (temperament travailleur)) (maison Poufsouffle))
   (R4 ((valeur sagesse) (sortilege Legilimens)) (maison Serdaigle))
 
-  (R5 ((qualité curieux) (boisson biere)) (qualite courage))
-  (R6 ((qualite adroit) (notes bon)) (qualite malice))
+  (R5 ((naturel curieux) (boisson biere)) (qualite courage))
+  (R6 ((karma oui) (boisson eau)) (qualite courage))
+  (R7 ((naturel adroit)) (qualite malice))
+  (R8 ((karma oui) (boisson vin)) (qualite malice))
 
-  (R7 ((animal chien) (karma non)) (temperament bagarreur))
-  (R8 ((milieu populaire) (fetard non)) (temperament travailleur))
+  (R9 ((valeur justice) (fetard oui)) (sortilege Expelliarmus))
+  (R10 ((naturel curieux) (fetard oui)) (sortilege Expelliarmus))
+  (R11 ((animal chat) (type introverti)) (sortilege Legilimens))
+  (R12 ((animal chat) (boisson vin)) (sortilege Legilimens))
 
-  (R9 ((karma oui) (type extraverti)) (valeur justice))
-  (R10 ((milieu bourgeois) (boisson vin)) (valeur sagesse))
+  (R13 ((animal chien) (karma non)) (temperament bagarreur))
+  (R14 ((boisson biere) (fetard oui)) (temperament bagarreur))
+  (R15 ((milieu populaire) (fetard non)) (temperament travailleur))
+  (R16 ((boisson eau) (fetard non)) (temperament travailleur))
 
-  (R11 ((valeur justice) (fetard oui)) (sortilege Expelliarmus))
-  (R12 ((animal chat) (type introverti)) (sortilege Legilimens))
+  (R17 ((karma oui) (type extraverti)) (valeur justice))
+  (R18 ((boisson vin) (animal chien)) (valeur justice))
+  (R19 ((milieu bourgeois) (boisson vin)) (valeur sagesse))
+  (R20 ((milieu bourgeois) (type introverti)) (valeur sagesse))
+
 ))
 
 ; Les valeurs de Poufsouffle sont la loyauté, la patience, la gentillesse, la modestie, le travail acharné, le fair-play, la persévérance la justice, la sincérité,, la tolérance, et l'amour de la nature.
